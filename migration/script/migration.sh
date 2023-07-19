05877056772620a6fa1ec1fa95fc2e147e4087c6
Kyramee <adrienthereader@gmail.com>
2023-07-19 15:48:46 -0400

    Update migration script

--- migration/script/migration.sh
+++ migration/script/migration.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-helm install -f migration/values.yaml migration ./migration/ --wait --wait-for-jobs > cleanup.sh 2> error.log
+helm install -f migration/values.yaml migration ./migration/ --wait --wait-for-jobs > cleanup.sh 2> migration-error.log
 chmod 770 cleanup.sh
 cat cleanup.sh | jq -r '.info.notes' > cleanup.sh
 
-helm install -f netbox/values.yaml -f migration/posgresql.yaml netbox ./netbox/ --wait
+echo "$(date) Installion error:" >> install-error.log
+helm install -f netbox/values.yaml -f migration/posgresql.yaml netbox ./netbox/ --wait > /dev/null 2>> netbox-error.log
+echo "$(date) Upgrade error:" >> install-error.log
+helm upgrade -f netbox/values.yaml netbox ./netbox/ --wait > /dev/null 2>> install-error.log
+
+./cleanup.sh
