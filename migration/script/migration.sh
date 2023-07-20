d8439e037f2109c8fec5c93730bcc2bfd7e9a135
Kyramee <adrienthereader@gmail.com>
2023-07-20 09:55:30 -0400

    Make script verbose

--- migration/script/migration.sh
+++ migration/script/migration.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+## Set working path at thr root of the chart
 CHART_PATH="$(dirname -- "${BASH_SOURCE[0]}")"       # relative
 CHART_PATH="$(cd -- "$CHART_PATH" && pwd)/../.."    # absolutized and normalized
 if [[ -z "$CHART_PATH" ]] ; then
@@ -9,13 +10,48 @@ if [[ -z "$CHART_PATH" ]] ; then
   exit 1  # fail
 fi
 
-helm install -f "$CHART_PATH/migration/values.yaml" migration "$CHART_PATH/migration/" --wait --wait-for-jobs > cleanup.sh 2> migration-error.log
+echo "Starting migration..."
+
+echo "$(date) Migration error:" >> $CHART_PATH/error.log
+
+helm install -f "$CHART_PATH/migration/values.yaml" migration "$CHART_PATH/migration/" --wait --wait-for-jobs > cleanup.sh 2> $CHART_PATH/error.log
+if [[ $? -eq 0 ]] ; then
+  echo "Migration failed: See $CHART_PATH/error.log for details"
+  exit 1  # fail
+if
+
 chmod 770 cleanup.sh
 cat cleanup.sh | jq -r '.info.notes' > cleanup.sh
 
-echo "$(date) Installion error:" >> install-error.log
-helm install -f netbox/values.yaml -f migration/posgresql.yaml netbox ./netbox/ --wait > /dev/null 2>> netbox-error.log
-echo "$(date) Upgrade error:" >> install-error.log
-helm upgrade -f netbox/values.yaml netbox ./netbox/ --wait > /dev/null 2>> install-error.log
+echo "Migration successful..."
+echo ""
+echo "Starting Netbox install..."
 
-./cleanup.sh
+echo "$(date) Netbox install error:" >> $CHART_PATH/error.log
+
+helm install -f netbox/values.yaml -f migration/posgresql.yaml netbox ./netbox/ --wait > /dev/null 2>> $CHART_PATH/error.log
+if [[ $? -eq 0 ]] ; then
+  echo "Netbox install failed: See $CHART_PATH/error.log for details"
+  exit 1  # fail
+if
+
+echo "Netbox install successful..."
+echo ""
+echo "Starting housekeeping..."
+
+echo "$(date) Housekeeping error:" >> $CHART_PATH/error.log
+
+helm upgrade -f netbox/values.yaml netbox ./netbox/ --wait > /dev/null 2>> $CHART_PATH/error.log
+if [[ $? -eq 0 ]] ; then
+  echo "Starting failed: See $CHART_PATH/error.log for details"
+  exit 1  # fail
+if
+
+$CHART_PATH/cleanup.sh > /dev/null 2>> $CHART_PATH/error.log
+if [[ $? -eq 0 ]] ; then
+  echo "Starting failed: See $CHART_PATH/error.log for details"
+  exit 1  # fail
+if
+
+echo "Housekeeping successful..."
+echo "Netbox is ready and operational!"
